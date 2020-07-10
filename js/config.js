64194b1b1d25cad086a79f748909ee73427c7ede
Ngo <tonyngo@3c22fb772fa9.ant.amazon.com>
2020-07-10 11:31:09 -0700

    cognito setup

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_y1eacbHaD', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3b4qpgv0q57ltjb7o01as19bpm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://we6hfgk72d.execute-api.us-east-1.amazonaws.com/pord' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
